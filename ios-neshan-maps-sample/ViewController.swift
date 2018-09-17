48835343f985095cdef853f5d14d105d2ec0ffd0
Mohammad-Reza Azizi <DSM@apple-nic-3.local>
2018-09-17 17:39:01 +0430

    Show map view

--- ios-neshan-maps-sample/ViewController.swift
+++ ios-neshan-maps-sample/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var mapview:NTMapView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        mapview = NTMapView();
+        let neshan = NTNeshanServices.createBaseMap(NTNeshanMapStyle.NESHAN)
+        mapview?.getLayers().add(neshan)
+        
+        let neshan2 = NTNeshanServices.createTrafficLayer()
+        mapview?.getLayers().add(neshan2)
+        
+        let neshan3 = NTNeshanServices.createPOILayer(false)
+        mapview?.getLayers().add(neshan3)
+        
+        mapview?.setFocalPointPosition(NTLngLat(x:59.2,y:36.5), durationSeconds: 0.4)
+        mapview?.setZoom(13, durationSeconds: 0.4)
+        view=mapview
     }
 
     override func didReceiveMemoryWarning() {
---
+++ ios-neshan-maps-sample/ios-neshan-maps-sample-bridging-header.m
@@ -0,0 +1,9 @@
+//
+//  ios-neshan-maps-sample-bridging-header.m
+//  ios-neshan-maps-sample
+//
+//  Created by Mohammad-Reza Azizi on 9/17/18.
+//  Copyright © 2018 Mohammad-Reza Azizi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
